994905771db840953e3319007fc850bc962cf87d
Fontaine Cook <fontaine.cook@pearance.com>
2012-05-04 02:05:33 -0400

    Update build script

--- scripts/pro_101_build.sh
+++ scripts/pro_101_build.sh
@@ -7,25 +7,29 @@ cd /srv/aegir/platforms/$1
 rm -r profiles/*
 ln -s ~/platforms/.profiles/pro_101 profiles/pro_101
 
-# Remove example modules and features.
-rm -r sites/all/modules/contrib/features/tests/
-
 # Remove unnecessary files.
+mv robots.txt sites/
+rm *.txt
+mv sites/robots.txt .
+rm install.php
+
 rm sites/all/README.txt
 rm sites/all/modules/README.txt
 rm sites/all/themes/README.txt
 rm sites/example.sites.php
 
+rm -r sites/all/modules/contrib/features/tests/
 rm -r sites/all/modules/contrib/uuid/uuid_services_example
+
+rm -r themes/stark
+rm -r themes/garland
+rm -r themes/seven
+
+
 
 # Make all features writable for ftool's direct save.
 chgrp -R www-data sites/all/modules/features/
 chmod -R 775 sites/all/modules/features/
-
-# Remove unnecessary core themes.
-rm -r themes/stark
-rm -r themes/garland
-rm -r themes/seven
 
 
 
