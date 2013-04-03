690dddcbc2201de9549f8d45dadd147312d6bdd4
Fontaine Cook <fontaine.cook@pearance.com>
2013-04-02 20:18:54 -0400

    Fix default pic path

--- scripts/post_site_install.sh
+++ scripts/post_site_install.sh
@@ -21,7 +21,7 @@ echo '----------------------------------------------------------------'
 /srv/aegir/bin/drush -y @$1 taxocsv-import https://raw.github.com/pearance/pro_101_install_profile/master/imports/qcategories.csv --keep_order --vocabulary_id=question_category --result_stats --result_terms
 
 # Copy default user pic.
-wget -P /srv/aegir/platforms/$1/sites/$1/images/user-pics/ https://raw.github.com/pearance/pro_101_install_profile/master/imports/picture-default.png
+wget -P /srv/aegir/platforms/$1/sites/$1/files/images/user-pics/ https://raw.github.com/pearance/pro_101_install_profile/master/imports/picture-default.png
 
 # Enable post install feature(s).
 /srv/aegir/bin/drush -y @$1 en pro_101_user_accounts
