3cf89edac300f73f54d6fe177cf32dcba5ff6ac4
Fontaine Cook <fontaine.cook@pearance.com>
2013-05-11 03:02:16 +0000

    Make paths user agnostic

--- scripts/pro_101_build.sh
+++ scripts/pro_101_build.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # vim: set ft=sh:
 
-drush make --working-copy ~/platforms/.profiles/pro_101/scripts/pro_101.make $1
+drush make --working-copy /srv/aegir/platforms/.profiles/pro_101/scripts/pro_101.make $1
 
 # Link installation profile.
-cd ~/platforms/$1
+cd /srv/aegir/platforms/$1
 rm -r profiles/*
-ln -s ~/platforms/.profiles/pro_101 profiles/pro_101
+ln -s /srv/aegir/platforms/.profiles/pro_101 profiles/pro_101
 
 # Remove unnecessary files.
 mv robots.txt sites/
@@ -30,14 +30,14 @@ chmod 775 sites/all/modules/features
 find sites/all/modules/features/ -type f -exec chmod 664 {} \;
 
 # Move custom views into place.
-/bin/cp -f ~/platforms/.profiles/pro_101/imports/workbench_moderation.view.inc sites/all/modules/contrib/workbench_moderation/views/.
+/bin/cp -f /srv/aegir/platforms/.profiles/pro_101/imports/workbench_moderation.view.inc sites/all/modules/contrib/workbench_moderation/views/.
 
 # Copy APC script to the build's scripts directory.
-/bin/cp ~/platforms/.profiles/pro_101/scripts/apc.php scripts/.
+/bin/cp /srv/aegir/platforms/.profiles/pro_101/scripts/apc.php scripts/.
 
 echo -e "\nPost build cleanup completed!"
 
 
 
 # Temporary hacks.
-touch ~/platforms/$1/sites/all/libraries/ckeditor/skins/moono/skin.js
+touch /srv/aegir/platforms/$1/sites/all/libraries/ckeditor/skins/moono/skin.js
