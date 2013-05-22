b8fa6dc055621b9a873579328f80d2f623e41157
Fontaine Cook <fontaine.cook@pearance.com>
2013-05-21 22:19:58 -0400

    Run script commands as aegir system account

--- scripts/post_site_install.sh
+++ scripts/post_site_install.sh
@@ -17,27 +17,27 @@ echo `date` - $1
 echo '----------------------------------------------------------------'
 
 # Import taxonomies.
-/srv/aegir/bin/drush -y @$1 taxocsv-import https://raw.github.com/pearance/pro_101_install_profile/master/imports/specialties.csv --keep_order --vocabulary_id=specialties --result_stats --result_terms
-/srv/aegir/bin/drush -y @$1 taxocsv-import https://raw.github.com/pearance/pro_101_install_profile/master/imports/qcategories.csv --keep_order --vocabulary_id=question_category --result_stats --result_terms
+su -s /bin/bash aegir -c "/srv/aegir/bin/drush -y @$1 taxocsv-import https://raw.github.com/pearance/pro_101_install_profile/master/imports/specialties.csv --keep_order --vocabulary_id=specialties --result_stats --result_terms"
+su -s /bin/bash aegir -c "/srv/aegir/bin/drush -y @$1 taxocsv-import https://raw.github.com/pearance/pro_101_install_profile/master/imports/qcategories.csv --keep_order --vocabulary_id=question_category --result_stats --result_terms"
 
 # Copy default user pic.
-wget -P /srv/aegir/platforms/$1/sites/$1/files/images/user-pics/ https://raw.github.com/pearance/pro_101_install_profile/master/imports/picture-default.png
+su -s /bin/bash aegir -c "wget -P /srv/aegir/platforms/$1/sites/$1/files/images/user-pics/ https://raw.github.com/pearance/pro_101_install_profile/master/imports/picture-default.png"
 
 # Enable post install feature(s).
-/srv/aegir/bin/drush -y @$1 en pro_101_user_accounts
+su -s /bin/bash aegir -c "/srv/aegir/bin/drush -y @$1 en pro_101_user_accounts"
 
 # Refresh all aliases. Must be executed before pro_101_wrap, which creates its
 # own path aliases.
-/srv/aegir/bin/drush    @$1 pathauto-aliases-delete
-/srv/aegir/bin/drush    @$1 pathauto-aliases-create
+su -s /bin/bash aegir -c "/srv/aegir/bin/drush    @$1 pathauto-aliases-delete"
+su -s /bin/bash aegir -c "/srv/aegir/bin/drush    @$1 pathauto-aliases-create"
 
 # Wrap it up and disable the update manager, enabled by default.
-/srv/aegir/bin/drush -y @$1 en pro_101_wrap
-/srv/aegir/bin/drush -y @$1 fr pro_101_wrap
-/srv/aegir/bin/drush -y @$1 dis update
-/srv/aegir/bin/drush -y @$1 pm-uninstall update
+su -s /bin/bash aegir -c "/srv/aegir/bin/drush -y @$1 en pro_101_wrap"
+su -s /bin/bash aegir -c "/srv/aegir/bin/drush -y @$1 fr pro_101_wrap"
+su -s /bin/bash aegir -c "/srv/aegir/bin/drush -y @$1 dis update"
+su -s /bin/bash aegir -c "/srv/aegir/bin/drush -y @$1 pm-uninstall update"
 
 # Verify site to rewrite vhost and enable boost caching.
-/srv/aegir/bin/drush @$1 provision-verify
+su -s /bin/bash aegir -c "/srv/aegir/bin/drush @$1 provision-verify"
 
 # vim: set ft=sh:
